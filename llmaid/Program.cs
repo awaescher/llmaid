2d7e9839f2116a2c2494b285d0adf464bdd20c89
awaescher <awaescher@gmx.de>
2024-10-16 20:51:17 +0200

    Enable usage with dotnet run

--- llmaid/Program.cs
+++ llmaid/Program.cs
@@ -15,7 +15,6 @@ internal static class Program
 		var cancellationToken = cancellationTokenSource.Token;
 
 		var config = new ConfigurationBuilder()
-			.SetBasePath(Directory.GetCurrentDirectory())
 			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
 			.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
 			.Build();
