a6bf7788dfde525aa49bfb98cdcb6bdf33369521
Andreas Wäscher <awaescher@gmx.de>
2024-12-10 16:49:33 +0100

    Add a vb code changer prompt

--- testfiles/frmMain.vb
+++ testfiles/frmMain.vb
@@ -1,6 +1,7 @@
 Imports System.Windows.Forms
 
 Public Class frmMain
+	Inherits Form
 
 	Private Sub btnPreview_Click(sender As Object, e As EventArgs) Handles btnPreview.Click
 		DocumentPreviewControl1.ShowPreview(txtPreviewFile.Text)
@@ -13,6 +14,21 @@ Public Class frmMain
 	''' <param name="e"></param>
 	Private Sub btnHidePreview_Click(sender As Object, e As EventArgs) Handles btnHidePreview.Click
 		DocumentPreviewControl1.HidePreview()
+
+		If Not Me.Visible And UpdateTitleAfter5Pm() Then
+			Me.Show()
+		End If
+
+		If Me.Visible And Not Me.Enabled Then
+			Me.Hide()
+		End If
+
+		If Me.Visible = False Then
+			Me.Enabled = False
+		Else
+			Me.Enabled = True
+		End If
+
 	End Sub
 
 	Private Sub btnFile_Click(sender As Object, e As EventArgs) Handles btnFile.Click
@@ -20,9 +36,19 @@ Public Class frmMain
 		dialog.Filter = "All files (*.*)|*.*"
 		dialog.FileName = txtPreviewFile.Text
 
-		If (dialog.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK) Then
+		If dialog.ShowDialog(Me) = System.Windows.Forms.DialogResult.OK And Not Me.Disposed Then
 			txtPreviewFile.Text = dialog.FileName
 		End If
 	End Sub
+
+	Public Function UpdateTitleAfter5Pm() As Boolean
+
+		If (DateTime.Now.Hour >= 17) Then
+			Me.Text = "After 5 PM"
+			Return True
+		End If
+
+		Return False
+	End Function
 
 End Class
