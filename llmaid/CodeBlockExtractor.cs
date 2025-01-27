1d6c0d3c697ba224ceaf7aab74e48bb872ce39fc
Andreas Wäscher <awaescher@gmx.de>
2025-01-27 09:52:25 +0100

    Fix parsing backtick codeblocks

--- llmaid/CodeBlockExtractor.cs
+++ llmaid/CodeBlockExtractor.cs
@@ -9,12 +9,12 @@ public static partial class CodeBlockExtractor
 
 	public static string Extract(string text, string tagNameForXmlBlock = "file")
 	{
-		if (!text.Contains($"<{tagNameForXmlBlock}>"))
-			return string.Empty;
-
-		var xmlMatch = new Regex($"<{tagNameForXmlBlock}>\\n?([\\s\\S]*?)\\n?<\\/{tagNameForXmlBlock}>").Matches(text).FirstOrDefault(m => m.Groups.Count > 1);
-		if (xmlMatch is not null)
-			return xmlMatch.Groups[1]?.Value.Trim() ?? string.Empty;
+		if (text.Contains($"<{tagNameForXmlBlock}>"))
+		{
+			var xmlMatch = new Regex($"<{tagNameForXmlBlock}>\\n?([\\s\\S]*?)\\n?<\\/{tagNameForXmlBlock}>").Matches(text).FirstOrDefault(m => m.Groups.Count > 1);
+			if (xmlMatch is not null)
+				return xmlMatch.Groups[1]?.Value.Trim() ?? string.Empty;
+		}
 
 		return MarkdownCodeBlockMatchPattern().Matches(text).FirstOrDefault(m => m.Groups.Count > 1)?.Groups[1]?.Value.Trim() ?? string.Empty;
 	}
